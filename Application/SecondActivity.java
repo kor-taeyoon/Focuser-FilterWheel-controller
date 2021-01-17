b0cab5374e4b45b87bed2d2b1d76d4034bdaf9ed
kimtaeyoon <dbsl0512@gmail.com>
2021-01-17 11:08:47 +0900

    Create SecondActivity.java

---
+++ Application/SecondActivity.java
@@ -0,0 +1,233 @@
+package com.example.focuser_controller;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SecondActivity extends AppCompatActivity {
+    // 변수
+    public int current_position = 0;
+    public int current_step = 0;
+    public int current_filter = 1;
+    public String[] filterArray = new String[] {"L", "R", "G (O3)", "B (S2)", "Ha"};
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 뷰 객체 탐색
+        TextView text_log = (TextView) findViewById(R.id.text_log);
+        TextView text_fc_step = (TextView) findViewById(R.id.text_fc_step);
+        TextView text_fc_position = (TextView) findViewById(R.id.text_fc_position);
+        TextView text_fw_current = (TextView) findViewById(R.id.text_fw_current);
+
+
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        Button btn_fc_in = (Button) findViewById(R.id.btn_fc_in) ;
+        Button btn_fc_out = (Button) findViewById(R.id.btn_fc_out) ;
+        Button btn_fc_up = (Button) findViewById(R.id.btn_fc_up) ;
+        Button btn_fc_down = (Button) findViewById(R.id.btn_fc_down) ;
+
+
+
+        // 포커서 좌표 갱신 핸들러, 타이머
+        @SuppressLint("HandlerLeak")
+        Handler handler = new Handler(){
+            public void handleMessage(Message msg){
+                text_fc_position.setText(Integer.toString(current_position));
+            }
+        };
+        Timer timer = new Timer(true);
+        TimerTask timerTask = new TimerTask(){
+            @Override
+            public void run(){
+                Message msg = handler.obtainMessage();
+                handler.sendMessage(msg);
+            }
+        };
+        timer.schedule(timerTask, 0, 20);
+
+
+
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // 이번트 리스너 설정 시작
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // fc IN 버튼 눌림 처리!
+        btn_fc_in.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        text_log.setText("들어가는 중..");
+                        current_position-=100;
+                        vibrator.vibrate(5);
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        text_log.setText("이동 끝!");
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+
+
+        // fc OUT 버튼 눌림 처리!
+        btn_fc_out.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        text_log.setText("나오는 중..");
+                        current_position+=100;
+                        vibrator.vibrate(5);
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        text_log.setText("이동 끝!");
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+        // fc up 버튼 눌림 처리!
+        btn_fc_up.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        vibrator.vibrate(5);
+                        if(current_step == 7){
+                            text_log.setText("더 증가시킬 수 없습니다.");
+                        }
+                        else{
+                            current_step++;
+                            text_fc_step.setText("uStep: 2^" + Integer.toString(current_step) + " = " + (int)Math.pow(2, current_step));
+                            text_log.setText("step up됨.");
+                        }
+                    }
+                }
+                return false;
+            }
+        });
+
+
+        // fc down 버튼 눌림 처리!
+        btn_fc_down.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        vibrator.vibrate(5);
+                        if(current_step == 0){
+                            text_log.setText("더 감소시킬 수 없습니다.");
+                        }
+                        else{
+                            current_step--;
+                            text_fc_step.setText("uStep: 2^" + Integer.toString(current_step) + " = " + (int)Math.pow(2, current_step));
+                            text_log.setText("step down됨.");
+                        }
+
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+        // fc init 버튼 눌림 처리!
+        Button btn_fc_init = (Button) findViewById(R.id.btn_fc_init);
+        btn_fc_init.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        text_log.setText("포커서가 초기화됨");
+                        current_position = 0;
+                        vibrator.vibrate(5);
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+
+        //여기부터는 필터휠!
+        // fw next 버튼 눌림 처리!
+        Button btn_fw_next = (Button) findViewById(R.id.btn_fw_next) ;
+        btn_fw_next.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        if(current_filter == 5){
+                            current_filter = 1;
+                        }
+                        else{
+                            current_filter++;
+                        }
+                        text_log.setText(filterArray[current_filter-1]+" 필터로 변경중입니다..");
+                        vibrator.vibrate(5);
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+        // fw pre 버튼 눌림 처리!
+        Button btn_fw_pre = (Button) findViewById(R.id.btn_fw_pre) ;
+        btn_fw_pre.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        if(current_filter == 1){
+                            current_filter = 5;
+                        }
+                        else{
+                            current_filter--;
+                        }
+                        text_log.setText(filterArray[current_filter-1]+" 필터로 변경중입니다..");
+                        vibrator.vibrate(5);
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+}
